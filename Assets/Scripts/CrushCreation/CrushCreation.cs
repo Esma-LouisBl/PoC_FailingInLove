e1f823e02f70f887786bd2537c94e79137a993d1
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2026-04-07 16:12:17 +0200

    Initialisation UI Création du Crush

---
+++ Assets/Scripts/CrushCreation/CrushCreation.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrushCreation : MonoBehaviour
+{
+    public Image hair, face, body, accessories;
+    public List<Sprite> hairSprites,  faceSprites, bodySprites, accessoriesSprites;
+    
+    private int hair
+
+    public void ChangeHair(bool increase)
+    {
+        if (increase)
+        {
+            hair.sprite =
+        }
+    }
+}
