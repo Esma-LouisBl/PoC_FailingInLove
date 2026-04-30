67fdba24dd31f7cc934cefc1202fa7330c072fa5
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2026-04-30 13:50:00 +0200

    Super bouton optimisé
    un peu fier de nous en vrai

--- Assets/Scripts/FastClickButton.cs
+++ Assets/Scripts/FastClickButton.cs
@@ -1,10 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class FastClickButton : MonoBehaviour, IPointerDownHandler
 {
+    [Serializable]
+    public class ButtonPressedEvent : UnityEvent {}
+    [SerializeField]
+    private ButtonPressedEvent m_OnPressed = new ButtonPressedEvent();
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("méga prout qui pue");
+        m_OnPressed.Invoke();
     }
 }
